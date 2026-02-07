08b89b8640af30f27b91a74906839c250f70076b
Michiel-ve <michiel.van.eynde123@gmail.com>
2026-02-07 21:07:04 +0200

    fixed career races endpoint to return it in order of calendar

--- src/main/java/be/michielve/f1_api/services/RaceService.java
+++ src/main/java/be/michielve/f1_api/services/RaceService.java
@@ -3,11 +3,13 @@ package be.michielve.f1_api.services;
 import be.michielve.f1_api.convertors.RaceConverter;
 import be.michielve.f1_api.models.response.RaceResponse;
 import be.michielve.f1_api.repositories.RaceRepository;
+import be.michielve.f1_api.models.Race;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,10 +23,13 @@ public class RaceService {
 
     public List<RaceResponse> getAllRacesForSeason(String season) {
         logger.info("Attempting to retrieve all races for season: {}", season);
-        List<RaceResponse> races = raceRepository.findAllRacesBySeasonName(season).stream()
+        
+        return raceRepository.findAllRacesBySeasonName(season).stream()
+                .sorted(Comparator.comparing(
+                    Race::getRaceStartDate, 
+                    Comparator.nullsLast(Comparator.naturalOrder())
+                ))
                 .map(raceConverter::raceWithSeasonResponse)
                 .collect(Collectors.toList());
-        logger.info("Successfully retrieved {} races for season: {}", races.size(), season);
-        return races;
     }
 }
