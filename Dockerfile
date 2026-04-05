3e354691d7ce387fd3e48c267454f08e0eb1f5f1
Michiel-ve <michiel.van.eynde123@gmail.com>
2026-04-05 12:48:22 +0300

    feat: Update Dockerfile and Gradle configuration for AWS Lambda integration, enhance dependency management, and configure JPA repositories

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM public.ecr.aws/lambda/java:21
 
-# The AWS image sets the WORKDIR to /var/task by default
+COPY build/libs/dependency/* ${LAMBDA_TASK_ROOT}/lib/
 COPY build/libs/f1_api.jar ${LAMBDA_TASK_ROOT}/lib/
 
-# Set the CMD to the handler string
 CMD ["be.michielve.f1_api.lambdas.LambdaHandler::handleRequest"]
--- src/main/java/be/michielve/f1_api/F1ApiApplication.java
+++ src/main/java/be/michielve/f1_api/F1ApiApplication.java
@@ -3,8 +3,12 @@ package be.michielve.f1_api;
 import be.michielve.f1_api.config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "be.michielve.f1_api.repositories")
+@EntityScan(basePackages = "be.michielve.f1_api.models")
 public class F1ApiApplication {
 
     public static void main(String[] args) {
