562cdf85c88e1622b4494cd4c2ac0c43ca9d63dc
Michiel-ve <michiel.van.eynde123@gmail.com>
2026-07-31 21:29:25 +0300

    update handler for lamnda

--- src/main/java/be/michielve/f1_api/lambdas/LambdaHandler.java
+++ src/main/java/be/michielve/f1_api/lambdas/LambdaHandler.java
@@ -5,6 +5,8 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import be.michielve.f1_api.F1ApiApplication;
 import be.michielve.f1_api.config.DotenvInitializer;
 
@@ -17,6 +19,12 @@ public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyRe
         
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(F1ApiApplication.class);
+            
+            // Disable FAIL_ON_EMPTY_BEANS directly on the container handler's ObjectMapper
+            if (handler.getObjectMapper() != null) {
+                handler.getObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+                handler.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            }
         } catch (Exception e) {
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
